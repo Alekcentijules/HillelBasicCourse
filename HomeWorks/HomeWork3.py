c39f96b6471f8253dc135d09c1ca36437e7e8970
O.Halaiev <o.halaiev@gmail.com>
2024-11-09 20:25:52 +0100

    Commit 3

--- HomeWorks/HomeWork3.py
+++ HomeWorks/HomeWork3.py
@@ -4,9 +4,9 @@ div, mod = divmod(user, 10000)
 div2, mod2 = divmod(mod, 1000)
 div3, mod3 = divmod(mod2, 100)
 div4, mod4 = divmod(mod3, 10)
-div5, mod5 = divmod(mod4, 1)
 
-result = div5 * 10000 + div4 * 1000 + div3 * 100 + div2 * 10 + div
+
+result = mod4 * 10000 + div4 * 1000 + div3 * 100 + div2 * 10 + div
 print("Your result: ", result)
 
 
