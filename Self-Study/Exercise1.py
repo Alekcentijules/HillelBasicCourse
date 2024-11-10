caa60ceae91ef43114592d99303550a6f6d370b9
O.Halaiev <o.halaiev@gmail.com>
2024-11-10 20:54:51 +0100

    Initial Commit

---
+++ Self-Study/Exercise1.py
@@ -0,0 +1,14 @@
+dollar_rate = float(input("Enter dollar rate: "))
+dollar_amount = int(input("Enter dollar amount to be converted to euro: "))
+
+convert = dollar_rate * dollar_amount
+
+print(f"Total amount in euro: {convert}")
+
+
+
+
+
+
+
+
---
+++ Self-Study/Exercise2.py
@@ -0,0 +1,12 @@
+user_input = int(input("Enter an integer: "))
+
+print(f'Binary representation: {bin(user_input)}')
+print(f'Octal representation: {oct(user_input)}')
+print(f'Hexadecimal representation: {hex(user_input)}')
+
+
+
+
+
+
+
