1debc19f1e78fb877543d46f20ce56048d34b5cd
O.Halaiev <o.halaiev@gmail.com>
2024-11-10 22:16:17 +0100

    Initial Commit

---
+++ Self-Study/Exercise3.py
@@ -0,0 +1,18 @@
+first_grade = int(input("Enter the first grade: "))
+second_grade = int(input("Enter the second grade: "))
+third_grade = int(input("Enter the third grade: "))
+
+average_score = (first_grade + second_grade + third_grade) / 3
+print(f'Average score: {average_score}')
+
+
+
+
+
+
+
+
+
+
+
+
