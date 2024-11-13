b35dc5da876074d270fd7525e6cd93e8aaa8e6c4
O.Halaiev <o.halaiev@gmail.com>
2024-11-13 10:27:45 +0100

    Commit 2

--- HomeWorks/HomeWork3.2.py
+++ HomeWorks/HomeWork3.2.py
@@ -1,13 +1,10 @@
-lst = [0, 1, 2, 3, 4, 5, 6]
+lst = [0, 1, 2, 3, 4, 5]
 
 if not lst:
     print([])
 
-elif len(lst) == 1:
-    print(lst)
-
 elif lst:
-    reverse_lst = [lst[-1]] + lst[1:-1] + [lst[0]]
+    reverse_lst = [lst[-1]] + lst[0:-1]
     print(reverse_lst)
 
 
