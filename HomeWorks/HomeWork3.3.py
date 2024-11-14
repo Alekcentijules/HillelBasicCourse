1dce06ab7ba1d975075657391d4a36fde27cfc5d
O.Halaiev <o.halaiev@gmail.com>
2024-11-14 14:17:08 +0100

    Initial Commit

---
+++ HomeWorks/HomeWork3.3.py
@@ -0,0 +1,25 @@
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+new_lst = []
+
+if len(lst) % 2 == 0:
+    part = len(lst) // 2
+    new_lst.append(lst[:part])
+    new_lst.append(lst[part:])
+    print(new_lst)
+
+elif len(lst) % 2 != 0:
+    part = len(lst) // 2 + 1
+    new_lst.append(lst[:part])
+    new_lst.append(lst[part:])
+    print(new_lst)
+
+
+
+
+
+
+
+
+
+
+
