9b0c9604011a4b6e376ec6a93c88d2e8fd62a0ab
O.Halaiev <o.halaiev@gmail.com>
2024-12-03 08:11:12 +0100

    Commit 2

--- HomeWorks/HomeWork7.3.py
+++ HomeWorks/HomeWork7.3.py
@@ -1,13 +1,14 @@
 def second_index(text, some_str):
     first_substr = text.find(some_str)
     second_text = text.find(some_str, first_substr + 1)
+    count_second_text = text.count(some_str)
+
+    # count_second_text = text.count(some_str) if text.count(some_str) >= 2 else return None
 
     if not text or not some_str:
         return 'The conversation requires at least two byte of text!'
-    elif second_text == -1:
-        return None
 
-    return second_text
+    return second_text if count_second_text >= 2 else None
 
 user_input_str = input("Enter a text: ")
 user_input_substr = input("Enter a text fragment to be found: ")
