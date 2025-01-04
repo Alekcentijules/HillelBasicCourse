15c811b7c7548121accfd7c39d836d7292fb2ca2
O.Halaiev <o.halaiev@gmail.com>
2025-01-04 20:02:34 +0100

    Commit 2

--- HomeWorks/HomeWork15.2.py
+++ HomeWorks/HomeWork15.2.py
@@ -45,10 +45,15 @@ class Fraction:
 f_a = Fraction(2, 3)
 f_b = Fraction(3, 6)
 f_c = f_b + f_a
+print(f_a)
+print(f_b)
+print(f_c)
 assert str(f_c) == 'Fraction: 21, 18'
 f_d = f_b * f_a
+print(f_d)
 assert str(f_d) == 'Fraction: 6, 18'
 f_e = f_a - f_b
+print(f_e)
 assert str(f_e) == 'Fraction: 3, 18'
 
 assert f_d < f_c  # True
@@ -56,6 +61,8 @@ assert f_d > f_e  # True
 assert f_a != f_b  # True
 f_1 = Fraction(2, 4)
 f_2 = Fraction(3, 6)
+print(f_1)
+print(f_2)
 assert f_1 == f_2  # True
 print('OK')
 
