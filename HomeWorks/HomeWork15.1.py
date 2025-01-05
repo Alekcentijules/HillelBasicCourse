be413a2ae347098119bef78b8744af9a0c3938fc
O.Halaiev <o.halaiev@gmail.com>
2025-01-05 17:12:49 +0100

    Commit 2

--- HomeWorks/HomeWork15.1.py
+++ HomeWorks/HomeWork15.1.py
@@ -5,11 +5,13 @@ class Rectangle:
         self.height = height
 
     def get_square(self):
-        return self.width * self.height
+        # return self.width * self.height
+        return round(self.width * self.height, 5)
 
     def __eq__(self, other):
         if isinstance(other, Rectangle):
-            return self.get_square() == other.get_square()
+            # return self.get_square() == other.get_square()
+            return round(self.get_square(), 5) == round(other.get_square(), 5)
         return NotImplemented
 
     def __add__(self, other):
@@ -18,12 +20,14 @@ class Rectangle:
             # sd_width = int(area ** 0.5) + 1
             # sd_height = area // sd_width
             # return Rectangle(sd_width, sd_height)
-            for sd_width in range(1, int(area ** 0.5) + 1):
-                if area % sd_width == 0:
-                    sd_height = area // sd_width
-                    return Rectangle(sd_width, sd_height)
-
-
+            # for sd_width in range(1, int(area ** 0.5) + 1):
+            #     if area % sd_width == 0:
+            #         sd_height = area // sd_width
+            #         return Rectangle(sd_width, sd_height)
+            sd_width = self.width
+            # sd_height = area / sd_width
+            sd_height = round(area / sd_width, 5)
+            return Rectangle(sd_width, sd_height)
         return NotImplemented
 
     def __mul__(self, n):
@@ -33,34 +37,53 @@ class Rectangle:
             # thd_height = area // thd_width
             # return Rectangle(thd_width, thd_height)
             # if isinstance(n, (int, float)):
-            area = int(self.get_square() * n)
-            for thd_width in range(1, int(area ** 0.5) + 1):
-                if area % thd_width == 0:
-                    thd_height = area // thd_width
-                    return Rectangle(thd_width, thd_height)
+            # area = int(self.get_square() * n)
+            # for thd_width in range(1, int(area ** 0.5) + 1):
+            #     if area % thd_width == 0:
+            #         thd_height = area // thd_width
+            #         return Rectangle(thd_width, thd_height)
+            thd_width = self.width
+            # thd_height = area / thd_width
+            thd_height = round(area / thd_width, 5)
+            return Rectangle(thd_width, thd_height)
         return NotImplemented
 
     def __str__(self):
         return f"Rectangle [ width = {self.width}, height = {self.height}, area = {self.get_square()} ]"
 
 
-r1 = Rectangle(2, 4)
+# r1 = Rectangle(2, 4)
+# r2 = Rectangle(3, 6)
+# print(r1)
+# print(r2)
+# assert r1.get_square() == 8, 'Test1'
+# assert r2.get_square() == 18, 'Test2'
+#
+# r3 = r1 + r2
+# print(r3)
+# assert r3.get_square() == 26, 'Test3'
+#
+# r4 = r1 * 4
+# print(r4)
+# assert r4.get_square() == 32, 'Test4'
+#
+# assert Rectangle(3, 6) == Rectangle(2, 9), 'Test5'
+
+r1 = Rectangle(2.4, 4)
 r2 = Rectangle(3, 6)
+
 print(r1)
 print(r2)
-assert r1.get_square() == 8, 'Test1'
-assert r2.get_square() == 18, 'Test2'
+# Test1
+assert r1.get_square() == 9.6, "Test1 failed"
+# Test2
+assert r2.get_square() == 18, "Test2 failed"
 
+# Test3: Складання прямокутників
 r3 = r1 + r2
 print(r3)
-assert r3.get_square() == 26, 'Test3'
-
-r4 = r1 * 4
-print(r4)
-assert r4.get_square() == 32, 'Test4'
-
-assert Rectangle(3, 6) == Rectangle(2, 9), 'Test5'
-
+assert r3.get_square() == 27.6, "Test3 failed"
+print('OK')
 
 
 
